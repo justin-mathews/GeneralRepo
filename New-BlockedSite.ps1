c135ec25c4dec5e1a342a5b6aab2ec5666b1a545
justin-mathews <justinmathews411@live.com>
2017-07-21 10:42:38 -0400

    Add files via upload

---
+++ New-BlockedSite.ps1
@@ -0,0 +1,36 @@
+﻿function New-HABlockedSite {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string[]]$DomainController,
+        [Parameter()]
+        [string[]]$Site
+    )
+    BEGIN {} # Begin
+    PROCESS {
+        foreach ($srv in $DomainController) {
+            Write-Verbose "Testing Connection to $srv"
+            if (Test-Connection -ComputerName $srv -Count 1 -Quiet) {
+                Write-Verbose "Connection successful"
+                foreach ($s in $Site) {
+                    try {
+                        $params = @{
+                            'ComputerName' = $srv
+                            'Name' = $s
+                            'ZoneFile' = "$s.dns"
+                            'ErrorAction' = 'Stop'
+                        }
+                        Add-DnsServerPrimaryZone @params
+                        Write-Verbose "Successfully created new zone: $s"
+                    } catch {
+                        Write-Warning "[Error]: An error has occurred while attempting to create the zone: $s"
+                        Write-warning "[Error]: $($_.Exception.Message)"
+                    } # try/catch Add-DnsServerPrimaryZone
+                } # foreach $site
+            } else {
+                Write-Warning "[Error]: Unable to contact $srv"
+            } # if/else Test-Connection
+        } # foreach $srv
+    } # Process
+    END {} # End
+} # function
